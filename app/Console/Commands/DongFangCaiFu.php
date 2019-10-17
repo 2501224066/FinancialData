4f4519a09daaa8007de3b988d64d75d32eb8807c
figurant <2501224066@qq.com>
2019-10-17 14:17:04 +0800

    东方财富类别内容空问题解决

--- app/Console/Commands/DongFangCaiFu.php
+++ app/Console/Commands/DongFangCaiFu.php
@@ -17,14 +17,14 @@ class DongFangCaiFu extends Command
 
     // 地址与分类
     protected $address_classify = [
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_102_ajaxResult_50_1_201910101256879319.html?r=0.'=> '全球直播',
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_zhiboall_ajaxResult_70_1_.html?r=0.'=> '股市直播', 
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_103_ajaxResult_50_1_.html?r=0.'=> '上市公司', 
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_105_ajaxResult_50_1_.html?r=0.'=> '全球股市', 
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_106_ajaxResult_50_1_.html?r=0.'=> '商品', 
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_107_ajaxResult_50_1_.html?r=0.'=> '外汇', 
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_108_ajaxResult_50_1_.html?r=0.'=> '基金', 
-        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_109_ajaxResult_50_1_.html?r=0.'=> '基金'
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_102_ajaxResult_50_1_201910101256879319.html?r=0.' => '全球直播',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_zhiboall_ajaxResult_70_1_.html?r=0.' => '股市直播',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_103_ajaxResult_50_1_.html?r=0.' => '上市公司',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_105_ajaxResult_50_1_.html?r=0.' => '全球股市',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_106_ajaxResult_50_1_.html?r=0.' => '商品',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_107_ajaxResult_50_1_.html?r=0.' => '外汇',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_108_ajaxResult_50_1_.html?r=0.' => '基金',
+        'http://newsapi.eastmoney.com/kuaixun/v1/getlist_109_ajaxResult_50_1_.html?r=0.' => '基金'
     ];
 
     public function __construct(NewsDfcf $newsDfcf)
@@ -35,22 +35,30 @@ class DongFangCaiFu extends Command
 
     public function handle()
     {
-        foreach($this->address_classify as $address => $classify){
-        // 目标地址
-        $url = $address . randNum(16) . "&_=" . getMillisecond();
-        $list = json_decode(substr(file_get_contents($url), 15))->LivesList;
+        foreach ($this->address_classify as $address => $classify) {
+            $url = $address . randNum(16) . "&_=" . getMillisecond();
+            $list = json_decode(substr(file_get_contents($url), 15))->LivesList;
 
-        foreach ($list as $v) {
-            $this->newsDfcf->updateOrCreate([
-                'seq' => $v->id
-            ], [
-                'title' => $v->title,
-                'content' => trim(substr($v->digest, strpos($v->digest, '】')), '】'),
-                'color' => $v->titlestyle == 0 ? 1 : 2,
-                'time' => $v->showtime,
-                'classify' => $classify
-            ]);
+            // 全球直播插入更新数据，其他地址仅更新分类
+            if ($classify == '全球直播') {
+                foreach ($list as $v) {
+                    $this->newsDfcf->updateOrCreate([
+                        'seq' => $v->id
+                    ], [
+                        'title' => $v->title,
+                        'content' => substr($v->digest, strpos($v->digest, '】') + 3),
+                        'color' => $v->titlestyle == 0 ? 1 : 2,
+                        'time' => $v->showtime,
+                        'classify' => $classify
+                    ]);
+                }
+            } else {
+                foreach ($list as $v) {
+                    $this->newsDfcf->where('seq', $v->id)->update([
+                        'classify' => $classify
+                    ]);
+                }
+            }
         }
-    }
     }
 }
